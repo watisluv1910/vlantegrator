0e83775649e43a4fc4b892ed07c5cbfdf292bda1
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2025-05-04 22:03:09 +0300

    fix(core): removed unused model

--- vlt-core/integrator/src/main/java/com/wladischlau/vlt/core/integrator/model/CommitInfo.java
+++
@@ -1,8 +0,0 @@
-package com.wladischlau.vlt.core.integrator.model;
-
-import lombok.Builder;
-
-import java.time.OffsetDateTime;
-
-@Builder
-public record CommitInfo(String hash, String author, String message, OffsetDateTime date) {}
