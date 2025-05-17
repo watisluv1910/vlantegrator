32e06adf4023d2b0e61e66e1613272f5235e7d2e
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2025-05-17 19:44:29 +0300

    fix(integrator): add required mode to RouteIdDto

--- vlt-core/commons/src/main/java/com/wladischlau/vlt/core/commons/dto/RouteIdDto.java
+++ vlt-core/commons/src/main/java/com/wladischlau/vlt/core/commons/dto/RouteIdDto.java
@@ -9,9 +9,9 @@ import java.util.UUID;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(description = "Полный идентификатор маршрута")
 public record RouteIdDto(
-        @Schema(description = "ID маршрута")
+        @Schema(description = "ID маршрута", requiredMode = Schema.RequiredMode.REQUIRED)
         @JsonProperty("id") UUID id,
-        @Schema(description = "Хэш-код версии маршрута")
+        @Schema(description = "Хэш-код версии маршрута", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
         @JsonProperty("versionHash") String versionHash) {
 
     public String full() {
