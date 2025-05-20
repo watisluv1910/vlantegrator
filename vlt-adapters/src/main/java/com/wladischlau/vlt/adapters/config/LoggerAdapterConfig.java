01e9ac86e85dfa054d77a79c6cac85c356904822
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2025-05-20 11:57:41 +0300

    feat: add openapi annotations to logger adapter config

--- vlt-adapters/src/main/java/com/wladischlau/vlt/adapters/config/LoggerAdapterConfig.java
+++ vlt-adapters/src/main/java/com/wladischlau/vlt/adapters/config/LoggerAdapterConfig.java
@@ -3,6 +3,8 @@ package com.wladischlau.vlt.adapters.config;
 import com.wladischlau.vlt.adapters.common.AdapterConfig;
 import com.wladischlau.vlt.adapters.utils.Slf4jLogLevelDeserializer;
 import com.wladischlau.vlt.adapters.utils.SpringExpressionDeserializer;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import org.slf4j.event.Level;
 
@@ -14,5 +16,7 @@ import java.util.List;
 public record LoggerAdapterConfig(
         @JsonDeserialize(using = Slf4jLogLevelDeserializer.class) Level logLevel,
         @NotBlank String msgTemplate,
-        @JsonDeserialize(contentUsing = SpringExpressionDeserializer.class) List<Expression> extractors
+        @JsonDeserialize(contentUsing = SpringExpressionDeserializer.class)
+        @ArraySchema(schema = @Schema(implementation = String.class, example = "\"$payload\""))
+        List<Expression> extractors
 ) implements AdapterConfig {}
