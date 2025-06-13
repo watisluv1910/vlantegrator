991a4c60f34f00dec5077dfd7a407ce9c025d391
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2025-06-13 22:28:27 +0300

    feat(core): add documentation server

---
+++ vlt-core/docs/src/main/java/com/wladischlau/vlt/core/docs/DocsServerApplication.java
@@ -0,0 +1,12 @@
+package com.wladischlau.vlt.core.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsServerApplication.class, args);
+    }
+}
